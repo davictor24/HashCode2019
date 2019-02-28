12d4194df5d022e513c02ca38b3c1e98f805ada1
Victor Durojaiye <durojaiyevic@gmail.com>
2019-02-28 19:48:17 +0100

    Wrote more code

--- solution.py
+++ solution.py
@@ -1,1 +1,48 @@
-print('Hello world')
+datasets = ['a_example']
+
+class Photo:
+    def __init__(self, photo_id, orientation, tags):
+        self.photo_id = photo_id
+        self.orientation = orientation
+        self.tags = tags
+
+class SlideShow:
+    def __init__(self):
+        self.slides = []
+        self._ids = []
+
+    def add_slide(self, photos):
+        self.slides.append(photos)
+        self._ids += [x.photo_id for x in photos]
+
+    def photo_exists(self, photo_id):
+        return photo_id in self._ids
+
+    def save(self, dataset):
+        f = open('outputs/' + dataset + '_solution.txt', 'w')
+        f.write(str(len(self.slides)) + '\n')
+        n = len(self.slides)
+        for i in range(n):
+            f.write(' '.join([str(x.photo_id) for x in self.slides[i]]))
+            if i < n-1: f.write('\n')
+
+def interest_factor(tags1, tags2):
+    return min(len(tags1.intersection(tags2)), len(tags1 - tags2), len(tags2 - tags1))
+
+for dataset in datasets:
+    photos = []
+    with open('datasets/' + dataset + '.txt') as f:
+        photo_count = int(f.readline())
+        for i in range(photo_count):
+            line = f.readline().rstrip()
+            linedata = line.split(' ')
+            photos.append(Photo(i, linedata[0], set(linedata[2:])))
+
+    slideshow = SlideShow()
+
+    for photo in photos:
+        slideshow.add_slide([photo])
+
+    slideshow.save()
+
+
